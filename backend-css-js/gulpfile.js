28c8ed6732a17314792b709cd94a482a88f6b45d
jotahtsx <joaomanoelskai@gmail.com>
2024-07-12 00:45:50 -0300

    feat: criando variáveis e gerando o sourcemap

--- backend-css-js/gulpfile.js
+++ backend-css-js/gulpfile.js
@@ -1,14 +1,17 @@
-const {src, dest, watch, series} = require('gulp')
-const sass = require('gulp-sass')(require('sass'))
+const { src, dest, watch, series } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
 
 function buildStyles() {
     return src('scss/main.scss')
-    .pipe(sass())
-    .pipe(dest('css'))
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('css'));
 }
 
 function watchTask() {
-    watch(['scss/main.scss'], buildStyles)
+    watch(['scss/**/*.scss'], buildStyles);
 }
 
-exports.default = series(buildStyles, watchTask)
+exports.default = series(buildStyles, watchTask);
