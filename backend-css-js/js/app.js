9ca055c7d67580e8811afc5ff8fbeb6c434a0ac5
jotahtsx <joaomanoelskai@gmail.com>
2024-07-30 00:05:27 -0300

    feat: inicializando dropdown content

--- backend-css-js/js/app.js
+++ backend-css-js/js/app.js
@@ -1,70 +1,41 @@
-const operateNavigations = (type, target, variables) => {
+const dropdown = () => {
+  const _variables = {
+    main: "vsb-dropdown",
+    menu: "vsb-dropdown__menu",
+    target: "data-dropdown-target",
+    active: "vsb-active",
+  }
 
-    let componentId = type === "dropdown" ? variables.target : variables.targetId;
-  
-    const targetId = type === "dropdown" ? target.querySelector(`[${componentId}]`).getAttribute(componentId) : target.getAttribute(componentId)
-  
+  const dropDown = [...document.querySelectorAll(`.${_variables.main}`)]
+
+  document.addEventListener("click", (vsb) => {
+
+    const target = vsb.target.closest(`.${_variables.main}`)
+
+    const targetedMenu = vsb.target.closest(`.${_variables.menu}`)
+
+    if (!target || targetedMenu) return
+
+    const targetId = target.querySelector(`[${_variables.target}]`).getAttribute(_variables.target)
+
     const activeMenu = document.querySelector(`#${targetId}`)
-  
-    const nonTargeted = variables.components.map(drop => {
-      const nonActiveId = drop.querySelector(`[${componentId}]`).getAttribute(componentId)
+
+    const nonTargeted = dropDown.map(drop => {
+      const nonActiveId = drop.querySelector(`[${_variables.target}]`).getAttribute(_variables.target)
       const nonActive = document.querySelector(`#${nonActiveId}`)
-  
+
       return nonActive
     })
-  
+
     const filterExceptActive = nonTargeted.filter(target => target !== activeMenu)
-  
-    filterExceptActive.forEach(drop => drop.classList.remove(variables.active))
-  
-    if (activeMenu) activeMenu.classList.toggle(variables.active)
-  
-  }
-  
-  // Fechando componentes
-  const closeComponents = (type, event, variables) => {
-  
-    const target = type === "dropdown" ? event.target.closest(`.${variables.menu}`) || event.target.closest(`[${variables.target}]`) : event.target.closest(`.${variables.menu}`) || event.target.closest(`.${variables.target}`)
-  
-    if (target) return
-  
-    variables.components.forEach(comp => {
-      const menu = comp.querySelector(`.${variables.menu}`)
-      if (menu.classList.contains(variables.active)) menu.classList.remove(variables.active)
-    })
-  
-  }
-  
-  const dropdown = () => {
-    const _variables = {
-      main: "vsb-dropdown",
-      menu: "vsb-dropdown__menu",
-      target: "data-dropdown-target",
-      active: "vsb-active",
-      components: [...document.querySelectorAll(`.vsb-dropdown`)]
-    }
-  
-    document.addEventListener("click", (vsb) => {
-  
-      const target = vsb.target.closest(`.${_variables.main}`)
-  
-      const targetedMenu = vsb.target.closest(`.${_variables.menu}`)
-  
-      if (!target || targetedMenu) return
-  
-      vsb.preventDefault()
-  
-      operateNavigations("dropdown", target, _variables);
-  
-    })
-  
-    window.addEventListener("mouseup", vsb => {
-  
-      closeComponents("dropdown", vsb, _variables);
-  
-    })
-  
-  }
-  
-  dropdown()
+
+    filterExceptActive.forEach(drop => drop.classList.remove(_variables.active))
+
+    if (activeMenu) activeMenu.classList.toggle(_variables.active)
+
+  })
+
+}
+
+dropdown()
 //# sourceMappingURL=app.js.map
