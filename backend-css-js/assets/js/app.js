11e823f29156d550cac90e7466c62df385b990cc
jotahtsx <joaomanoelskai@gmail.com>
2024-08-02 20:32:33 -0300

    feat: fechar o menu ao clicar fora

--- backend-css-js/assets/js/app.js
+++ backend-css-js/assets/js/app.js
@@ -9,32 +9,35 @@ const dropdown = () => {
   const dropDown = [...document.querySelectorAll(`.${_variables.main}`)]
 
   document.addEventListener("click", (vsb) => {
-
     const target = vsb.target.closest(`.${_variables.main}`)
-
     const targetedMenu = vsb.target.closest(`.${_variables.menu}`)
 
-    if (!target || targetedMenu) return
+    if (!target && !targetedMenu) {
+      dropDown.forEach(drop => {
+        const targetId = drop.querySelector(`[${_variables.target}]`).getAttribute(_variables.target)
+        const menu = document.querySelector(`#${targetId}`)
+        if (menu) menu.classList.remove(_variables.active)
+      })
+      return
+    }
 
-    const targetId = target.querySelector(`[${_variables.target}]`).getAttribute(_variables.target)
+    if (target && !targetedMenu) {
+      const targetId = target.querySelector(`[${_variables.target}]`).getAttribute(_variables.target)
+      const activeMenu = document.querySelector(`#${targetId}`)
 
-    const activeMenu = document.querySelector(`#${targetId}`)
+      const nonTargeted = dropDown.map(drop => {
+        const nonActiveId = drop.querySelector(`[${_variables.target}]`).getAttribute(_variables.target)
+        const nonActive = document.querySelector(`#${nonActiveId}`)
+        return nonActive
+      })
 
-    const nonTargeted = dropDown.map(drop => {
-      const nonActiveId = drop.querySelector(`[${_variables.target}]`).getAttribute(_variables.target)
-      const nonActive = document.querySelector(`#${nonActiveId}`)
+      const filterExceptActive = nonTargeted.filter(target => target !== activeMenu)
 
-      return nonActive
-    })
+      filterExceptActive.forEach(drop => drop.classList.remove(_variables.active))
 
-    const filterExceptActive = nonTargeted.filter(target => target !== activeMenu)
-
-    filterExceptActive.forEach(drop => drop.classList.remove(_variables.active))
-
-    if (activeMenu) activeMenu.classList.toggle(_variables.active)
-
+      if (activeMenu) activeMenu.classList.toggle(_variables.active)
+    }
   })
-
 }
 
 dropdown()
